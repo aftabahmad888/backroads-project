dd772d5a6b35c330131eff3fb25edb1441391990
Aftab Ahmad <aftabahmad.farcy.888@gmail.com>
2024-11-05 23:25:22 +0000

    I created the Links.js file for the data

---
+++ src/components/Linkss.js
@@ -0,0 +1,12 @@
+export const Linkss = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#services", text: "services" },
+  { id: 3, href: "#tours", text: "tours" },
+  { id: 4, href: "#about", text: "about" },
+];
+export const socialmedia = [
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 2, href: "https://www.twitter.com", icon: "fab fa-facebook" },
+  { id: 3, href: "https://www.twitter.com", icon: "fab fa-instagram" },
+  { id: 4, href: "https://www.twitter.com", icon: "fab fa-snapchat" },
+];
---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import logo from "../images/logo.svg";
+import { Linkss, socialmedia } from "./Linkss";
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <img src={logo} className="nav-logo" alt="backroads" />
+            <button type="button" className="nav-toggle" id="nav-toggle">
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+
+          {/* This is for the navbar links  */}
+          <ul className="nav-links" id="nav-links">
+            {Linkss.map((items) => {
+              return (
+                <li key={items.id}>
+                  <a href={items.href} className="nav-link">
+                    {items.text}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+
+          {/* This is for the social media links */}
+          <ul className="nav-icons">
+            {socialmedia.map((items) => {
+              const { id, href, icon } = items;
+              return (
+                <li key={id}>
+                  <a href={href} target="_blank" className="nav-icon">
+                    <i className={icon}></i>
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
